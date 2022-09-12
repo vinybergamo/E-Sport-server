bb7a96550c1417184dbb2d938deda06a36863841
vinybergamo <vinybergamo@gmail.com>
2022-09-11 21:11:26 -0300

    Typescript Configured

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/ads", (req, res) => {
+  return res.json({ msg: "Success" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Servidor ativo");
+});
