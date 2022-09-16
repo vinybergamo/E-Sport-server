04013875aa831288e32345456505ffe5c94d2e94
vinybergamo <vinybergamo@gmail.com>
2022-09-16 11:22:07 -0300

    Server update

--- src/server.ts
+++ src/server.ts
@@ -13,10 +13,6 @@ app.get("/games", (req, res) => {
   return res.json({ msg: "2" });
 });
 
-app.post("/games", (req, res) => {
-  return res.json({ msg: "2" });
-});
-
 app.listen(process.env.PORT, () => {
   console.log("Servidor ativo");
 });
