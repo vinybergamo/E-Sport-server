2a86726fcb0a7139c86d193e2dc4df595676f285
vinybergamo <vinybergamo@gmail.com>
2022-09-16 17:24:30 -0300

    Listening Discord Incremented

--- src/server.ts
+++ src/server.ts
@@ -56,8 +56,19 @@ app.get("/games/:id/ads", async (req, res) => {
   );
 });
 
-app.get("/ads/:id/discord", (req, res) => {
-  return res.json([]);
+app.get("/ads/:id/discord", async (req, res) => {
+  const adId = req.params.id;
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return res.json({ discord: ad.discord });
 });
 
 app.listen(process.env.PORT, () => {
