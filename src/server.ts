c8f9174ba20d7fbe9aac139dd68bf4a93b4314d1
vinybergamo <vinybergamo@gmail.com>
2022-09-16 17:19:47 -0300

    Listening Ads List Incremented

--- src/server.ts
+++ src/server.ts
@@ -25,14 +25,35 @@ app.post("/ads", (req, res) => {
   return res.status(201).json([]);
 });
 
-app.get("/games/:id/ads", (req, res) => {
-  return res.json([
-    { id: "1", name: "Anúncio 1" },
-    { id: "2", name: "Anúncio 2" },
-    { id: "3", name: "Anúncio 3" },
-    { id: "4", name: "Anúncio 4" },
-    { id: "5", name: "Anúncio 5" },
-  ]);
+app.get("/games/:id/ads", async (req, res) => {
+  const gameId = req.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekdays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return res.json(
+    ads.map((ad) => {
+      return {
+        ...ad,
+        weekdays: ad.weekdays.split(","),
+      };
+    })
+  );
 });
 
 app.get("/ads/:id/discord", (req, res) => {
