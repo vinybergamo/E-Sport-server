7fb0b331617cef8b738759ce2b5c66f7496cba52
vinybergamo <vinybergamo@gmail.com>
2022-09-16 12:45:28 -0300

    Created Routes

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,26 @@ dotenv.config();
 
 const app = express();
 
-app.get("/ads", (req, res) => {
-  return res.json({ msg: "1" });
-}); 
-
 app.get("/games", (req, res) => {
-  return res.json({ msg: "2" });
+  return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  return res.json([
+    { id: "1", name: "Anúncio 1" },
+    { id: "2", name: "Anúncio 2" },
+    { id: "3", name: "Anúncio 3" },
+    { id: "4", name: "Anúncio 4" },
+    { id: "5", name: "Anúncio 5" },
+  ]);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+  return res.json([]);
 });
 
 app.listen(process.env.PORT, () => {
