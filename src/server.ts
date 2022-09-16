c286154dec2ce47524009634ca2b191025634014
vinybergamo <vinybergamo@gmail.com>
2022-09-16 14:10:43 -0300

    Update Database

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ app.post("/ads", (req, res) => {
 
 app.get("/games/:id/ads", (req, res) => {
   return res.json([
-    { id: "1", name: "Anúncio " },
+    { id: "1", name: "Anúncio 1" },
     { id: "2", name: "Anúncio 2" },
     { id: "3", name: "Anúncio 3" },
     { id: "4", name: "Anúncio 4" },
